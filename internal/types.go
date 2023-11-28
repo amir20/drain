77e844a1eae6e9c01cf8ef07ba45bc0f02628a3b
Amir Raminfar <findamir@gmail.com>
2023-11-28 09:16:25 -0800

    adds json

--- internal/types.go
+++ internal/types.go
@@ -3,16 +3,16 @@ package internal
 import "time"
 
 type Event struct {
-	CreatedAt         time.Time
-	AuthProvider      string
-	RemoteIP          string
-	Version           string
-	Clients           int
-	HasDocumentation  bool
-	FilterLength      int
-	HasCustomAddress  bool
-	HasCustomBase     bool
-	HasHostname       bool
-	RunningContainers int
-	Browser           string
+	CreatedAt         time.Time `json:"createdAt"`
+	AuthProvider      string    `json:"authProvider"`
+	RemoteIP          string    `json:"remoteIp"`
+	Version           string    `json:"version"`
+	Clients           int       `json:"clients"`
+	HasDocumentation  bool      `json:"hasDocumentation"`
+	FilterLength      int       `json:"filterLength"`
+	HasCustomAddress  bool      `json:"hasCustomAddress"`
+	HasCustomBase     bool      `json:"hasCustomBase"`
+	HasHostname       bool      `json:"hasHostname"`
+	RunningContainers int       `json:"runningContainers"`
+	Browser           string    `json:"browser"`
 }
