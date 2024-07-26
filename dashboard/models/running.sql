70f66b729acb6a7477cdb248b0b9d60b862a3155
Amir Raminfar <findamir@gmail.com>
2024-07-26 15:20:21 -0700

    updates dashboards

---
+++ dashboard/models/running.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT on (CASE WHEN ServerID != '' THEN ServerID ELSE RemoteIP END,  date_trunc('day', CreatedAt)) date_trunc('day', CreatedAt) as CreatedAt, AuthProvider, "Version",  HasCustomAddress, HasCustomBase, IsSwarmMode, HasActions, ServerVersion, RemoteAgents, "RemoteClients", Mode from start ORDER by RemoteIP, CreatedAt desc
