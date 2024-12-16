536f49ab863151a2ebe84ecd60016cd6633723c8
Amir Raminfar <findamir@gmail.com>
2024-12-16 08:30:49 -0800

    adds pg and grafana

--- internal/types.go
+++ internal/types.go
@@ -3,24 +3,24 @@ package internal
 import "time"
 
 type Event struct {
-	Name              string
-	CreatedAt         time.Time
-	AuthProvider      string
-	RemoteIP          string
-	Version           string
-	Clients           int
-	FilterLength      int
-	HasCustomAddress  bool
-	HasCustomBase     bool
-	HasHostname       bool
-	RunningContainers int
-	Browser           string
-	HasActions        bool
-	IsSwarmMode       bool
-	ServerVersion     string
-	ServerID          string
-	Mode              string
-	RemoteAgents      int
-	RemoteClients     int
-	SubCommand        string
+	Name              string    `json:"name"`
+	CreatedAt         time.Time `json:"createdAt"`
+	AuthProvider      string    `json:"authProvider"`
+	RemoteIP          string    `json:"remoteIP"`
+	Version           string    `json:"version"`
+	Clients           int       `json:"clients"`
+	FilterLength      int       `json:"filterLength"`
+	HasCustomAddress  bool      `json:"hasCustomAddress"`
+	HasCustomBase     bool      `json:"hasCustomBase"`
+	HasHostname       bool      `json:"hasHostname"`
+	RunningContainers int       `json:"runningContainers"`
+	Browser           string    `json:"browser"`
+	HasActions        bool      `json:"hasActions"`
+	IsSwarmMode       bool      `json:"isSwarmMode"`
+	ServerVersion     string    `json:"serverVersion"`
+	ServerID          string    `json:"serverID"`
+	Mode              string    `json:"mode"`
+	RemoteAgents      int       `json:"remoteAgents"`
+	RemoteClients     int       `json:"remoteClients"`
+	SubCommand        string    `json:"subCommand"`
 }
