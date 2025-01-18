2ad9af59383fed9e6f23f8b5c8db595d01e2bcc8
Amir Raminfar <findamir@gmail.com>
2025-01-18 15:38:30 -0800

    adds new views

--- init/01_init.sql
+++ init/01_init.sql
@@ -22,6 +22,8 @@ CREATE INDEX idx_name_client_time ON beacon (name, client_id, time);
 
 CREATE INDEX idx_metadata ON beacon USING GIN (metadata);
 
+CREATE INDEX idx_beacon_time_name_client ON beacon (time, name, client_id);
+
 -- Setup compression
 ALTER TABLE beacon
 SET
@@ -48,3 +50,61 @@ GROUP BY
     1
 ORDER BY
     2;
+
+CREATE MATERIALIZED VIEW daily_client_events
+WITH
+    (timescaledb.continuous) AS
+SELECT
+    time_bucket ('1 day', time) as day,
+    client_id,
+    (array_agg(metadata ORDER BY time DESC))[1] as last_metadata,
+    max(time) as last_seen
+FROM
+    beacon
+WHERE
+    name = 'events'
+GROUP BY
+    1,
+    2;
+
+SELECT add_continuous_aggregate_policy('daily_client_events',
+    start_offset => INTERVAL '1 year',
+    end_offset => INTERVAL '1 hour',
+    schedule_interval => INTERVAL '30 minutes'
+);
+
+CREATE INDEX idx_daily_client_events_day
+ON daily_client_events(day);
+
+CREATE INDEX idx_daily_client_events_client
+ON daily_client_events(client_id, day);
+
+CREATE INDEX idx_daily_client_events_metadata
+ON daily_client_events USING GIN (last_metadata);
+
+CREATE MATERIALIZED VIEW daily_client_starts
+WITH (timescaledb.continuous) AS
+SELECT
+    time_bucket('1 day', time) as day,
+    client_id,
+    (array_agg(metadata ORDER BY time DESC))[1] as last_metadata,
+    max(time) as last_seen
+FROM beacon
+WHERE name = 'start'
+GROUP BY 1, 2;
+
+-- Add refresh policy
+SELECT add_continuous_aggregate_policy('daily_client_starts',
+    start_offset => INTERVAL '1 year',
+    end_offset => INTERVAL '1 hour',
+    schedule_interval => INTERVAL '30 minutes'
+);
+
+CREATE INDEX idx_daily_client_starts_day
+ON daily_client_starts(day);
+
+CREATE INDEX idx_daily_client_starts_client
+ON daily_client_starts(client_id, day);
+
+CREATE INDEX idx_daily_client_starts_metadata
+ON daily_client_starts USING GIN (last_metadata);
